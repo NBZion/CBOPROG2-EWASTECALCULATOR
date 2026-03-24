c08b753c908a7ce5c4865a191947ccc25f4e7692
NBZion <subtopewds0921@gmail.com>
2026-03-24 22:18:53 +0800

    feat: Update login.h

--- include/login.h
+++ include/login.h
@@ -1,6 +1,20 @@
 #ifndef LOGIN_H
 #define LOGIN_H
 
+#include <stdio.h>
 
+#define STR_LEN 50
+#define MAX_USER 10
 
+typedef char String[STR_LEN] ;
+
+typedef struct {
+    String user;
+    String password;
+    char admin; // t if true f if false
+} User;
+
+void saveFile(User database[], int size, FILE *f);
+void loadFile(User database[], int *size, FILE *f);
+void run();
 #endif
