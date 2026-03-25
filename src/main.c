1181367462af1da6fc5e5ae69d514471330fff13
Kexrus <kexrusosu@gmail.com>
2026-03-25 21:08:09 +0800

    Added the original main function of devices.c to main.c
    additional: #include "devices.c"

--- src/main.c
+++ src/main.c
@@ -1,7 +1,25 @@
 #include "login.h"
-
+#include "devices.c"
 int main() {
     run();
+
+    
+    FILE *fDevices;
+    FILE *fMinerals;
+    fDevices = fopen("devices.txt", "r");
+    fMinerals = fopen("minerals.txt", "r");
+
+    deviceInfo devices[MAX_DEVICES];
+
+    if (fDevices != NULL && fMinerals != NULL)
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            initializeDevices(&devices[i], fDevices, fMinerals);   // Initializes the deviceName and mineralType for every element of devices[]
+        }
+    }
+    
+    
 
     return 0;
 }
