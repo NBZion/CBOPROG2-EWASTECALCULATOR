ed362f14f29162aa63cb4d6050969f3b3b5e1d5c
NBZion <subtopewds0921@gmail.com>
2026-03-25 19:51:52 +0800

    feat: Add encrypt & decrypt

--- src/encrypt.c
+++ src/encrypt.c
@@ -1,1 +1,26 @@
+#include <string.h>
 #include "encrypt.h"
+
+
+// Vigenere Cipher
+void encrypt(String destination, const String source) {
+    int keyLen = (int)strlen(KEY);
+    int i;
+
+    for(i=0; source[i] != '\0'; i++) {
+        destination[i] = (source[i] -33 + KEY[i % keyLen]) % 94 + 33;
+    }
+
+    destination[i] = '\0';
+}
+
+void decrypt(String destination, const String source) {
+    int keyLen = (int)strlen(KEY);
+    int i;
+
+    for(i=0; source[i] != '\0'; i++) {
+        destination[i] = (source[i] - 33 - KEY[i % keyLen] + 94) % 94 + 33;
+    }
+
+    destination[i] = '\0';
+}
