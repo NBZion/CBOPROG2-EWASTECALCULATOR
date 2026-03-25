19e624f5a32cb801f944ca4cbdba6812067df106
NBZion <subtopewds0921@gmail.com>
2026-03-25 18:13:18 +0800

    feat: prepare encrypt.h

--- include/encrypt.h
+++ include/encrypt.h
@@ -1,4 +1,11 @@
 #ifndef ENCRYPT_H
 #define ENCRYPT_H
 
+#include "login.h"
+
+#define KEY "SEGFAULT"
+
+int encrypt(String destination, const String source);
+int decrypt(String destination, const String source);
+
 #endif
