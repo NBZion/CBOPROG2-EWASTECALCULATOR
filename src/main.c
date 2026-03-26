62474d1a9833857edd232bd9401484b2134eab36
NBZion <subtopewds0921@gmail.com>
2026-03-26 09:04:27 +0800

    refactor: comment out main.c for now

--- src/main.c
+++ src/main.c
@@ -4,23 +4,23 @@
 int main() {
     run();
 
-    
-    FILE *fDevices;
-    FILE *fMinerals;
-    fDevices = fopen("devices.txt", "r");
-    fMinerals = fopen("minerals.txt", "r");
 
-    deviceInfo devices[MAX_DEVICES];
+    // FILE *fDevices;
+    // FILE *fMinerals;
+    // fDevices = fopen("devices.txt", "r");
+    // fMinerals = fopen("minerals.txt", "r");
 
-    if (fDevices != NULL && fMinerals != NULL)
-    {
-        for (int i = 0; i < 100; i++)
-        {
-            initializeDevices(&devices[i], fDevices, fMinerals);   // Initializes the deviceName and mineralType for every element of devices[]
-        }
-    }
-    
-    
+    // deviceInfo devices[MAX_DEVICES];
+
+    // if (fDevices != NULL && fMinerals != NULL)
+    // {
+    //     for (int i = 0; i < 100; i++)
+    //     {
+    //         initializeDevices(&devices[i], fDevices, fMinerals);   // Initializes the deviceName and mineralType for every element of devices[]
+    //     }
+    // }
+
+
 
     return 0;
 }
