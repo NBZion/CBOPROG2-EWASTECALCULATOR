0e1e7c3ce82a632144d58b690de95c28c71c6498
Kexrus <kexrusosu@gmail.com>
2026-03-27 11:57:01 +0800

    added device initialization to pLoop

--- src/program.c
+++ src/program.c
@@ -2,9 +2,27 @@
 #include <string.h>
 #include "program.h"
 #include "login.h"
+#include "devices.h"
 
 void pLoop(User currentUser)
 {
     printf("Welcome %s!\n", currentUser.user);
+
+    // Device initialization
+    FILE *fDevices;
+    FILE *fMinerals;
+    fDevices = fopen("devices.txt", "r");
+    fMinerals = fopen("minerals.txt", "r");
+
+    deviceInfo devices[MAX_DEVICES];
+
+    if (fDevices != NULL && fMinerals != NULL)
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            initializeDevices(&devices[i], fDevices, fMinerals);
+        }
+    }
+    
     return;
 }
