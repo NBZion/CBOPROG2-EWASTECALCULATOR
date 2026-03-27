6ef26b5148b8e6e915d41c6f50c8b96657bd2d5d
Kexrus <kexrusosu@gmail.com>
2026-03-27 11:51:33 +0800

    removed device initialization from main func

--- src/main.c
+++ src/main.c
@@ -3,23 +3,5 @@
 int main() {
     run();
 
-
-    // FILE *fDevices;
-    // FILE *fMinerals;
-    // fDevices = fopen("devices.txt", "r");
-    // fMinerals = fopen("minerals.txt", "r");
-
-    // deviceInfo devices[MAX_DEVICES];
-
-    // if (fDevices != NULL && fMinerals != NULL)
-    // {
-    //     for (int i = 0; i < 100; i++)
-    //     {
-    //         initializeDevices(&devices[i], fDevices, fMinerals);   // Initializes the deviceName and mineralType for every element of devices[]
-    //     }
-    // }
-
-
-
     return 0;
 }
