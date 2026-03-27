ef041b6fc7d0b2007c78db26e7aa82d9456836af
NBZion <subtopewds0921@gmail.com>
2026-03-27 21:41:35 +0800

    feat: prepare header file for device database

--- include/devices.h
+++ include/devices.h
@@ -1,6 +1,9 @@
 #ifndef DEVICES_H
 #define DEVICES_H
 
+#include <stdio.h>
+#include <login.h>
+
 #define MAX_DEVICES 200     // There are 100 devices in the dataset, which means that the admin can only add up to 100 more devices
 
 typedef struct mineralType
@@ -17,11 +20,19 @@ typedef struct mineralType
 
 typedef struct deviceInfo
 {
-    char deviceName[51];    // Name of the device
+    String deviceName;    // Name of the device
     mineralType minerals;   // Minerals contained within the device
     float price;            // Calculated price of minerals contained within the device
 }deviceInfo;
 
+typedef struct {
+    String name;
+    deviceInfo devices[10];
+    int deviceCount;
+} UserDevice;
+
 void initializeDevices(deviceInfo *d, FILE *devices, FILE *minerals);
+void loadDevice(UserDevice database[], int *size, FILE *f);
+void saveDevice(UserDevice database[], int *size, FILE *f);
 
 #endif
